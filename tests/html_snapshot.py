977fc61f7b422d6693672951926f33ae88c59a83
Joshua Fehler <jsfehler@gmail.com>
2019-06-28 15:04:43 -0400

    Add another unit test for browser.html_snapshot()

--- tests/html_snapshot.py
+++ tests/html_snapshot.py
@@ -16,3 +16,12 @@ class HTMLSnapshotTest(object):
         """Should add the suffix to the snapshot filename"""
         filename = self.browser.html_snapshot(suffix="xml")
         self.assertEqual("xml", filename[-3:])
+
+    def test_html_snapshot_content_correct(self):
+        """The content of an HTML snapshot should match the actual page."""
+        html = self.browser.html
+
+        filename = self.browser.html_snapshot()
+        with open(filename, 'r') as f:
+            snapshot = f.read()
+        self.assertEqual(snapshot, html)
